b42f7db8280137e4cb6af23ef2990db80e788734
Bryan Ortiz <ortiz.b.t@gmail.com>
2022-02-19 16:33:45 -0600

    added ability to use Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/widgets.dart';
+import 'package:wod_generator/firebase_options.dart';
 import 'package:wod_generator_repository/wod_generator_repository.dart';
 import 'app.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(App(
     wodGeneratorRepository: WodGeneratorRepository(),
   ));
