750bc6bb16a16c130084cec078970f66e0143e3c
Bryan Ortiz <ortiz.b.t@gmail.com>
2022-02-21 23:28:47 -0600

    fix path

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/widgets.dart';
 import 'package:wod_generator_repository/wod_generator_repository.dart';
 import 'app.dart';
-import 'firebase_options.dart';
+import '../firebase_options.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
