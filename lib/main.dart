201f11653eac5c4c5e304beaa307f3dd66dcce8f
Bryan Ortiz <ortiz.b.t@gmail.com>
2022-02-21 23:15:22 -0600

    make sure to fix import statement

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/widgets.dart';
-import 'package:wod_generator/firebase_options.dart';
 import 'package:wod_generator_repository/wod_generator_repository.dart';
 import 'app.dart';
+import 'firebase_options.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
